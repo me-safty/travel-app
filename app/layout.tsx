033976d82c6217e621c548bbdece582924093e12
me-safty <me.a.safty@gmail.com>
2025-06-12 22:02:40 +0300

    make the session provider client untill fix the server one problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import { ReactNode } from "react";
-import "./globals.css";
+import { ReactNode } from "react"
+import "./globals.css"
+import Providers from "@/app/providers"
 // شلت البروفايدر اصلا  مبدئيا حتى تلاقي حل للموضوع
 
-export default function RootLayout({ children }: {children:ReactNode}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
-  );
+  )
 }
